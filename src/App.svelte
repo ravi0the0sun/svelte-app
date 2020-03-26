44ed0df4099bba96653462b66175e380dc94dd2f
Ravi <punravi73@gmail.com>
2020-03-26 15:47:04 +1100

    logic change

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,8 @@
 	const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 	const signs = ['+','-','/','*'];
 	let sign;
-	let number = [];
+	let number1;
+	let number2;
 	let output = 0
 
 	function assign(sign){
@@ -11,23 +12,25 @@
 	
 	function handleClick() {
 		switch(sign) {
-			case('+') : output = number[0] + number[1]; break;
-			case('-') : output = number[0] - number[1]; break;
-			case('*') : output = number[0] * number[1]; break;
-			case('/') : output = number[0] / number[1]; break;
+			case('+') : output = number1 + number2; break;
+			case('-') : output = number1 - number2; break;
+			case('*') : output = number1 * number2; break;
+			case('/') : output = number1 / number2; break;
 		}
 	}
 </script>
 
 <main>
-	{#each num as i}
-		<button class="btn">{i}</button>
-	{/each}
-	{#each signs as sign}
-		<button class="btn" >{sign}</button>
-	{/each}
-	<button class="btn" on:click={handleClick} >=</button>
-	<p>{output}</p>
+	<select bind:value={number1}>{#each num as number}
+		<option>{number}</option>
+	{/each}</select>
+	<select bind:value={sign}>{#each signs as sign}
+		<option>{sign}</option>
+	{/each}</select>
+	<select bind:value={number2}>{#each num as number}
+		<option>{number}</option>
+	{/each}</select>
+	= {output}
 </main>
 
 <style></style>
